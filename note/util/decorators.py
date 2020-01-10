f6c14f2cdba9b9dcaef5eea9b164d092de6f4613
unknown <zhdashuai@foxmail.com>
2020-01-10 15:19:36 +0800

    test

--- note/util/decorators.py
+++ note/util/decorators.py
@@ -9,16 +9,21 @@ def delete_file(func):
     @wraps(func)
     def decorated(*args, **kwargs):
         # 数据库文件列表
-        file_dir = Upload.query.with_entities(Upload.file_name)
+        # file_dir = Upload.query.with_entities(Upload.file_name)
+        upload = Upload.query.all()
+        file_dir = []
+        for visit in upload:
+            file_dir.append(visit.file_name)
         # 当前文件路径
         basepath = os.path.dirname(__file__)
         # 文件所在路径
         dirpath = os.path.join(basepath[:-5], 'static/upload_file')
         for filename in os.listdir(dirpath):
             if filename not in file_dir:
-                file_delete = os.path.join(basepath[:-6], 'static/upload_file',
+                file_delete = os.path.join(basepath[:-5], 'static/upload_file',
                                            filename)
                 if os.path.exists(file_delete):
                     os.remove(file_delete)
         return func(*args, **kwargs)
+
     return decorated
